0cd8da5654f00051be54c784bbf0357e59ad2e01
Tiago José <tiagoleite.work@gmail.com>
2024-06-11 18:57:52 +0000

    W3 PHP Variables complete

---
+++ dump.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>PHP Learning</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <h3>Julhiiiiiiiiiiiinha</h3>
+    <?php
+    // one comment Line
+    # one comment Line
+    /* multiple comment
+    lines */
+    $numero = 1 /* in the middle */ + 1;
+    echo $numero,"<br>";
+    $julhinha = "Julhinha"; //hey
+    echo "Hellow ". $julhinha . "<br>";
+    ECHO "Hello Juinha2"
+    ?>
+  </body>
+</html>
